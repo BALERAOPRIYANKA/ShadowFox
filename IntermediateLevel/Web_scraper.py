ebb8213258083cd7912c70f1d6d72233622938d4
Priyanka balerao Priya <priyankabaleraopriya@gmail.com>
2026-07-25 21:20:45 +0530

    Update Web_scraper.py

--- IntermediateLevel/Web_scraper.py
+++ IntermediateLevel/Web_scraper.py
@@ -1,1 +1,20 @@
 
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://quotes.toscrape.com/"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    title = soup.title.string
+    print("Page Title:", title)
+
+    with open("page_title.txt", "w") as file:
+        file.write(title)
+
+    print("Title saved successfully in page_title.txt")
+else:
+    print("Failed to fetch the webpage.")
